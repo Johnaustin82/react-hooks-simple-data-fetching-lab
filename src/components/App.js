535589e6284b8ab856e43a1e00c305ea7c5d945f
John Austin Ngode <austinngode23@gmail.com>
2024-05-07 03:33:48 +0300

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+     .then(response => response.json())
+     .then(data => {
+        setImageUrl(data.message);
+        setLoading(false);
+      })
+     .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div>
+      {loading? (
+        <p>Loading...</p>
+      ) : (
+        <img src={imageUrl} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
